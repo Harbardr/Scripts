ff79b7f403d244760adeadf7c5d1e24dc7350c3e
Harbardr <jfern.paris@gmail.com>
2016-04-11 13:39:05 +0200

    Update create_repository.sh

--- create_repository.sh
+++ create_repository.sh
@@ -103,7 +103,7 @@ function template_authz
     sed -i "s/LEAD_TEMPLATE/$LEAD_TEMPLATE/g" $OUTFILE
     sed -i "s/SUB_TEMPLATE/$SUB_TEMPLATE/g" $OUTFILE
     
-    sed -i 's/USERS_LIST/$(<users.list)/g' $OUTFILE
+    sed -e 's/USERS_LIST/ {r users.list d}' -i $OUTFILE
     #sed -i "s/USERS_LIST/$USERS_FILE/g" $OUTFILE
 }
 
