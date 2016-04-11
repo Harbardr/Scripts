16edcb5dccd7d7bfd7ba97d06efeff2955c6aa54
Harbardr <jfern.paris@gmail.com>
2016-04-11 15:50:48 +0200

    Update create_repository.sh

--- create_repository.sh
+++ create_repository.sh
@@ -278,7 +278,7 @@ if [ "$interactive" = "1" ]; then
                     for i in "${RESP[@]}"; do
                         # process "$i"
                         #printf "    %s:%s\n" "${RESP[$i]}" "${arrayUsers[${RESP[$i]}]}"
-                        printf "    %d:%s:%s\n" "$i" "${RESP[$((i-1))]}" "${arrayUsers[${RESP[$((i))]}]}"
+                        printf "    %d:%s\n" "$i" "${arrayUsers[${RESP[$((i))]}]}"
                     done
                 done <<< $response
                 #while IFS=',' read -r -a RESP; do
