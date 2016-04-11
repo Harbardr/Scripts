c82e6b32228e3a1c3ab237859d5240e89e5cf535
Harbardr <jfern.paris@gmail.com>
2016-04-11 16:54:52 +0200

    Update create_repository.sh

--- create_repository.sh
+++ create_repository.sh
@@ -87,7 +87,7 @@ function template_authz
     LEAD_TEMPLATE="\\&${LEAD_TEMPLATE//,/,\\&}"
     SUB_TEMPLATE="\\&${SUB_TEMPLATE//,/,\\&}"
     
-    USERS_FILE="users.list"
+    USERS_FILE="$6"
     
     #rm $OUTFILE
     cp $TEMPLATE $OUTFILE
@@ -102,7 +102,9 @@ function template_authz
     sed -i "s/PROJECT_TEMPLATE/$PROJECT_TEMPLATE/g" $OUTFILE
     sed -i "s/LEAD_TEMPLATE/$LEAD_TEMPLATE/g" $OUTFILE
     sed -i "s/SUB_TEMPLATE/$SUB_TEMPLATE/g" $OUTFILE
-    sed -e '/USERS_LIST/ {' -e 'r $USERS_FILE' -e 'd' -e '}' -i $OUTFILE
+    sed -e '/USERS_LIST/ {' -e 'r users.list' -e 'd' -e '}' -i $OUTFILE
+    
+    
 }
 
 function usage
